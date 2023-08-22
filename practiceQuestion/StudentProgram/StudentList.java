2e01b11a9e96725709d1231da9b8caf6bcbf2beb
Eden-truong <122903948+Eden-truong@users.noreply.github.com>
2023-08-21 21:35:00 -0400

    StudentList.java
    finalized

--- practiceQuestion/StudentProgram/StudentList.java
+++ practiceQuestion/StudentProgram/StudentList.java
@@ -3,6 +3,15 @@ package ITEC2150.practiceQuestion.StudentProgram;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * class: StudentList
+ * author: Eden Truong
+ * date: 08/19/2023
+ * version: 1.0
+ * description: ITEC 2150 Practice Student Array List
+ *
+ */
+
 public class StudentList {
     private List<Student> students;
 
